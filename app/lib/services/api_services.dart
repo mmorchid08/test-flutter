b2df3c6bd074d34b066acbe8b5fb642967b3312f
Amine Morchid <aminemorchid@Amines-MacBook-Pro.local>
2022-05-23 00:23:15 +0100

    error handling

--- app/lib/services/api_services.dart
+++ app/lib/services/api_services.dart
@@ -1,27 +1,33 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:app/models/till_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiServices {
-
   static Future<TillModel?> getOrders() async {
     TillModel? result;
     try {
-      final response = await http.get(
-        Uri.parse(
-            "https://raw.githubusercontent.com/popina/test-flutter/main/data.json"),
-      );
+      final response = await http
+          .get(
+            Uri.parse(
+                "https://raw.githubusercontent.com/popina/test-flutter/main/data.json"),
+          )
+          .timeout(const Duration(seconds: 30));
+
       if (response.statusCode == 200) {
         final item = json.decode(response.body);
         result = TillModel.fromMap(item);
-      } 
-      else {
-        print("error");
+      } else {
+        throw Exception('Error getting data');
       }
-    } catch (e) {
-      print(e);
-      throw Exception();
+    } on SocketException {
+      throw Exception('No internet connection');
+    } on TimeoutException catch (e) {
+      throw Exception('Connection timeout');
+    } on Error catch (e) {
+      throw Exception('Error getting data');
     }
     return result;
   }
